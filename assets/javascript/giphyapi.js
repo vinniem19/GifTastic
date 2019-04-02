25273624c3af74ca675f9bcf8e24ba38af208e01
unknown <cuzvin2@gmail.com>
2019-04-02 09:45:21 -0400

    Buttons appear when user clicks submit; linked css correctly

--- assets/javascript/giphyapi.js
+++ assets/javascript/giphyapi.js
@@ -1,56 +1,82 @@
 
 // My array of Holidays
-var topic = ["Christmas", "Easter", "4th of July", "New Year"];
+let topic = ["Christmas", "Easter", "4th of July", "New Year"];
 console.log(topic);
 
-// This stores the query url as a variable
+    
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Fpy46RCZ5r7l1RWFL71kJzbyOTffX7Ig&q=christmas&limit=10&rating=pg";
-
-// I will need an onclick function to hold all that data until the click
-      $("button").on("click", function (event) {
+  
+    // When user clicks submit, the button is added (using the renderButtons function below) 
+      $("#add-holiday").on("click", function (event) {
       event.preventDefault();
   // This line of code will grab the input from the textbox
-      var holiday = $("#holiday-input").val().trim();
+      let holidayInput = $("#holiday-input").val().trim();
+        $("#holidays-input").empty();
+        // The holiday from the user input is then added to our array
+      topic.push(holidayInput);
+      
+      renderButtons();
+      });
 
-// The movie from the textbox is then added to our array
-      topic.push(holiday);
-// Putting this array into buttons on a page
-// This will create the onClick event listener to make the holiday button clickable
+    // This will be my function that displays the data after push of specific button
+         function displayGiphy(topic) {
+      
+      // This stores the query url as a variable
+            let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Fpy46RCZ5r7l1RWFL71kJzbyOTffX7Ig&q=" + topic + "&limit=10&rating=pg";
+      
+      //  This is the ajax call method to get the information from the Giphy api
+             $.ajax({
+             url: queryURL,
+             method: "GET"
+            }).then(function(response) { // This defines the function to get the data as a response
+            
+            let results = response.data;
+            console.log(results);
+            // I am looping through the data and prepending images of the different holidays
+            for (let i = 0; i < results.length; i++) {
+                let gifDiv = $("<div>");
+                let rating = results[i].rating;
+                let para = $("<p>").text("Rating: " + rating);
+                let gifImage1 = $("<img>");
+                gifImage1.addClass("active-gif");
+                let imageUrl = results[i].images.fixed_height.url;
+                gifImage1.attr("src", imageUrl);
+                
+                
+                gifDiv.prepend(para).append(gifImage1);
+                
+                // Here we put the div with the holiday image, p, and rating before the images already created. 
+                $("#holiday-view").prepend(gifDiv);
+            };
+        });
+     };
+    // Function for making the buttons appear in the html and as part of the topic array (working-movie-app)
 
-    //  This is the ajax call method to get the information from the Giphy api
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) { // This defines the function to get the data as a response
+function renderButtons() {
 
-        let results = response.data;
-        console.log(results);
-      // I am looping through the data and prepending images of the different holidays
-     for (let i = 0; i < results.length; i++) {
-        let gifDiv = $("<div>");
+    // Deletes the new holiday buttons
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+    // Loops through the array of movies
+    for (let i = 0; i < topic.length; i++) {
 
-        let rating = results[i].rating;
-
-        let p = $("<p>").text("Rating: " + rating);
-
-        let holidayImage = $("<img>");
-        var imageUrl = results[i].images.fixed_height.url;
-
-        // This variable will hold the html image element
-        // This creates the source attribute for the holiday image var and provide alt text tag
-        holidayImage.attr("src", imageUrl);
-        holidayImage.attr("alt", "holiday image");
-        
-
-        gifDiv.prepend(p);
-        gifDiv.prepend(holidayImage);
-
-        // Here we put the div with the holiday image, p, and rating before the images already created. 
-            $("#holiday-view").prepend(gifDiv);
-      }
-        
-
+      // Then dynamicaly generates buttons for each movie in the array
+      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+      let holidayButtonAdd = $("<button>");
+      // Adds a class of movie to our button
+      holidayButtonAdd.addClass("holiday");
+      // Added a data-attribute
+      holidayButtonAdd.attr("data-name", topic[i]);
+      // Provided the initial button text
+      holidayButtonAdd.text(topic[i]);
+      // Added the button to the buttons-view div
+      $("#buttons-view").append(holidayButtonAdd);
+    }
+  }
+  renderButtons();
+  
+    // I will need an onclick function to hold all this data until the click
     
-      })
-    })
+    
+    
+    
